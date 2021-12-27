6b04b0bf33142da68c760925c91ede445895de25
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2021-12-27 12:34:02 +0530

    Time: 26 ms (55.92%), Space: 47.2 MB (92.28%) - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.java
+++ k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,8 +1,28 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points,(a,b)-> squareDistance(a)-squareDistance(b));
-            
-            return Arrays.copyOf(points,k);
+    //     Arrays.sort(points,(a,b)-> squareDistance(a)-squareDistance(b));
+    //         return Arrays.copyOf(points,k);
+    // }
+    
+        
+        
+        Queue<int []> maxPQ = new PriorityQueue<>((a,b)-> b[0]-a[0]);
+        for(int i=0;i<points.length;i++){
+            int entry[] = {squareDistance(points[i]),i};
+            if(maxPQ.size()<k){
+                maxPQ.add(entry);
+            }else if(entry[0]<maxPQ.peek()[0]){
+                maxPQ.poll();
+                maxPQ.add(entry);
+            }
+        }
+        
+        int ans[][] = new int [k][2];
+        for(int i=0;i<k;i++){
+            int entryIndex = maxPQ.poll()[1];
+            ans[i] = points[entryIndex];
+        }
+        return ans;
     }
     private int squareDistance(int[] point){
         return point[0]*point[0] + point[1]*point[1];
