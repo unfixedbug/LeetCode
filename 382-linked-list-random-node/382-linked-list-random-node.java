27d1926d89f50ac10b702d5220fc541f689f8bdf
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-01-07 20:53:11 +0530

    Time: 20 ms (20.09%), Space: 47.6 MB (9.65%) - LeetHub

--- 382-linked-list-random-node/382-linked-list-random-node.java
+++ 382-linked-list-random-node/382-linked-list-random-node.java
@@ -8,11 +8,44 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
+    Random rand;
+    ListNode head;
+    int n;
+    
+    public Solution(ListNode head) {
+        this.rand  = new Random();
+        this.head = head;
+        this.n = 0;
+        
+        ListNode it = head;
+        while(it!=null){
+            ++n;
+            it = it.next;
+        }
+    }
+    
+    public int getRandom() {
+        ListNode iter = head;
+        int k = rand.nextInt(n);
+        
+            while(iter !=null){
+                if(k==0){
+                    return iter.val;
+                }
+                iter = iter.next;
+                --k;
+            }
+        return -1;
+    }
+}
+
+class Solution2 {
     private ArrayList<Integer> range = new ArrayList<>();
     
     
-    public Solution(ListNode head) {
+    public Solution2(ListNode head) {
         while(head!=null){
             this.range.add(head.val);
             head = head.next;
