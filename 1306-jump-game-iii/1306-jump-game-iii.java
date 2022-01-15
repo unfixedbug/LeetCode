0d1b79d2dcdebdc4a65428f9e0b0ba1dc4109f2a
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-01-15 11:47:43 +0530

    Time: 3 ms (64.70%), Space: 56.3 MB (19.30%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,5 +1,21 @@
 class Solution {
-    public boolean canReach(int[] arr, int start) {
+    
+//     recursive solution BFS
+    public boolean canReach(int []arr, int start){
+        if(start < 0 || start >= arr.length || arr[start] < 0) {
+            return false;
+        } 
+        if (arr[start] == 0) {
+            return true;
+        }
+        arr[start] = -arr[start];
+        
+        return canReach(arr, start - arr[start]) || canReach(arr, start + arr[start]);
+    }
+    
+    
+    
+    public boolean canReachIterative(int[] arr, int start) {
         
         Queue<Integer> q = new LinkedList<>();
         q.add(start);
