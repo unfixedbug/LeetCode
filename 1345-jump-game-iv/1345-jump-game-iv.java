52cf02371d07b12551cb64253a032076d6b9a647
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-01-15 13:29:23 +0530

    Time: 85 ms (44.43%), Space: 115.5 MB (27.28%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.java
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -5,48 +5,44 @@ class Solution {
         if(n<=1) return 0;
         
         Map<Integer ,List<Integer>> graph  = new HashMap<>(); // value, same valued indexes
-        for(int i=0; i<n ; i++){
-            graph.computeIfAbsent(arr[i], v-> new LinkedList<>()).add(i);
-            
-//                                 value, (indexes with value (value))
+        for(int i=0;i<n;i++){
+            if(!graph.containsKey(arr[i])){
+                graph.put(arr[i], new ArrayList<>());
+            }
+            graph.get(arr[i]).add(i);
         }
-            List<Integer>curs = new LinkedList<>(); //store current layer
-            curs.add(0);
-            Set<Integer> visited = new HashSet<>();
-            int step=0;
-            
-            while(!curs.isEmpty()){
-                List<Integer> nex = new LinkedList<>();
-                
-                for(int node: curs){
-//                     check if reched thee end
-                    if(node ==n-1) return step;
-                    
-                    //check whether same value exist?
-                    for(int child: graph.get(arr[node])){ // iterating thorught values of graph
-                        if(!visited.contains(child)){
-                            visited.add(child);
-                            nex.add(child); //next layer
+        
+        Queue<Integer> q = new LinkedList<>();
+        
+        boolean[]visited = new boolean[n];
+        q.add(0);
+        visited[0]=true;
+        
+        int steps=0;
+        while(!q.isEmpty()){
+            for(int k=0, size = q.size(); k< size;k++){//iterating thorugh all indices with same value
+                int i=q.remove(); // remove, or poll the remove
+                if(i== n-1) return steps;
+                if(i-1 >=0 && !visited[i-1]){
+                    q.add(i-1);
+                    visited[i-1]=true;
+                }
+                if(i+1 < n && !visited[i+1]){
+                    q.add(i+1);
+                    visited[i+1]=true;
+                }
+                if(graph.containsKey(arr[i])){
+                    for(int j: graph.get(arr[i])){
+                        if(!visited[j]){
+                            q.add(j);
+                            visited[j]=true;
                         }
                     }
-
-                    // clear the list, ie all values of the node, to prevent redundant searches
-                   graph.get(arr[node]).clear();
-                    
-                    // check neighbours
-                    if(!visited.contains(node+1)  && node +1 < n){
-                        visited.add(node+1);
-                        nex.add(node+1);
-                    }
-                    if(!visited.contains(node-1) && node -1 >=0){
-                        visited.add(node-1);
-                        nex.add(node -1);
-                    }
+                    graph.remove(arr[i]);
                 }
-                curs  =nex;
-                step++;
             }
-            
+            steps++;
+        }
         return -1;
     }
 }
