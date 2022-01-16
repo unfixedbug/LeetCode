2849fada796eeb50e0c0646a9b60d2a48b303df0
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-01-16 21:50:56 +0530

    Time: 6 ms (12.30%), Space: 47.2 MB (6.30%) - LeetHub

---
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxDistToClosest(int[] seats) {
+        
+        int n = seats.length;
+        int []left = new int[n], right = new int[n];
+        Arrays.fill(left, n);
+        Arrays.fill(right,n);
+        
+        for(int i=0; i<n;i++){
+            if(seats[i]==1) left[i]=0;
+            else if(i>0) left[i]= left[i-1]+1; // previous pointer  +1
+        }
+        
+        for(int i=n-1; i>= 0;i--){
+            
+            if(seats[i]==1) right[i]=0;
+            else if(i < n-1) right[i]= right[i+1] + 1; // previous pointer  +1
+        
+        }
+        
+        int ans=0;
+        
+        for(int i=0; i<n; i++){
+            
+            if(seats[i] ==0){
+                ans = Math.max(ans, Math.min(left[i], right[i]));
+            }
+        }
+        return ans;
+        
+    }
+}
