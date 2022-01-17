6b2f085fa51f82efb9c18ce6077dd57c2d333775
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-01-17 21:24:51 +0530

    Time: 62 ms (40.47%), Space: 39 MB (48.73%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,23 +1,22 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
 //         n^2 dp
-        int dp[] = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            dp[i]=1;
-        }
-        int max = 0;
-        for(int i=1;i<nums.length;i++){
+        int n=nums.length;
+        int ans=0;
+        int max = Integer.MIN_VALUE;
+        int dp[] = new int [n];
+        for(int i=0;i<n;i++){
+            dp[i]=1;}
+        for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-//                 dp[i] will be the last eelemtn
-                if(nums[i]>nums[j] && dp[j]+1 > dp[i] ){
+                if(nums[i]>nums[j] && dp[i]<dp[j]+1){
                     dp[i]=dp[j]+1;
                 }
             }
-            
         }
-        for (int i = 0; i < nums.length; i++){
-            if (max < dp[i]){
-                max = dp[i];}}
+        for (int i = 0; i < n; i++)
+            if (max < dp[i])
+                max = dp[i];
  
         return max;
     }
