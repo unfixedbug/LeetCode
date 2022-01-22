c1ad0e14ed19202ae338d3be122c94b114ff079b
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-01-22 18:11:46 +0530

    Time: 20 ms (19.68%), Space: 44.6 MB (27.98%) - LeetHub

---
+++ 877-stone-game/877-stone-game.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        int dp[][] = new int[n+2][n+2];
+//         dp[i+1][j+1] = value of the game
+        for(int size = 1;size<=n;size++){
+            for(int i=0; i + size <=n;i++){
+                int j= i+ size-1;
+                int p = (j+i+n)%2;
+                
+                if(p==1){
+                    dp[i+1][j+1] = Math.max(piles[i]+ dp[i+2][j+1], piles[j]+dp[i+1][j]);
+                    
+                }
+                else{
+                    dp[i+1][j+1] = Math.min(-piles[i]+ dp[i+2][j+1], -piles[j] + dp[i+1][j]);
+                }
+            }
+        }
+        return dp[1][n]>0;
+    
+    }
+}
