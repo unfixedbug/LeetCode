dfd74c9bf278ba7e40b9cb298f8e15b06e8b5cf2
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-01-23 16:26:45 +0530

    Time: 0 ms (100.00%), Space: 36.1 MB (95.11%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -1,15 +1,39 @@
 class Solution {
+    
+    
     List<Integer> ans;
     int lo, hi;
     public List<Integer> sequentialDigits(int low, int high) {
-        ans = new ArrayList<>();
+//         nau=ive soln
+     String digits = "123456789";
+        ans= new ArrayList<>();
+        
+        for (int len = 2; len <= digits.length(); len++) {
+            for (int j = 0; j + len <= digits.length(); j++) {
+                String sub = digits.substring(j, j + len);
+                Integer num = Integer.parseInt(sub);
+                if (num >= low && num <= high) {
+                    ans.add(num);
+                }
+            }
+        }
+        
+        
+//         for recursive soln
+        /*
+        
+        
         lo=low;
         hi=high;
         for(int i=1;i<10;i++){
             go(i);
         }
         Collections.sort(ans);
-        return ans;
+       
+        */
+            
+             return ans;
+       
     }
     
     void go(int num){
