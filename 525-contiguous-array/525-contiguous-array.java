d5023550b5a039dbf441d7c8e8c05c19f143fb56
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-02-04 08:19:39 +0530

    Time: 38 ms (45.24%), Space: 84.7 MB (52.23%) - LeetHub

---
+++ 525-contiguous-array/525-contiguous-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0,-1);
+        int maxlen=0,count=0; // if count==0, equal amount of nbumbers
+        for(int i=0 ; i < nums.length;i++){
+            count += nums[i]==1?1:-1;
+            if(map.containsKey(count)){
+                maxlen = Math.max(maxlen, i- map.get(count)); // this index - last index with ans 0;
+            }
+            else {
+                map.put(count,i);
+            }
+        }
+        return maxlen;
+    }
+}
