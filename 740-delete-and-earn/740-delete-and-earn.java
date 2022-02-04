5293eac7e7e5377d251388e26bb3a66c01993bff
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-02-04 11:27:46 +0530

    Time: 4 ms (63.88%), Space: 45.1 MB (8.87%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -0,0 +1,21 @@
+class Solution {
+   
+    public int deleteAndEarn(int[] nums) {
+        int[] count = new int[10001];
+        for (int x: nums) count[x]++;
+        int avoid = 0, using = 0, prev = -1;
+
+        for (int k = 0; k <= 10000; ++k) if (count[k] > 0) {
+            int m = Math.max(avoid, using);
+            if (k - 1 != prev) {
+                using = k * count[k] + m;
+                avoid = m;
+            } else {
+                using = k * count[k] + avoid;
+                avoid = m;
+            }
+            prev = k;
+        }
+        return Math.max(avoid, using);
+    }
+}
