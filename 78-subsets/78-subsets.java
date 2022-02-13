a4a4e1cad5941b750e2a831751b32fe0c4c08505
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-02-13 09:57:21 +0530

    Time: 1 ms (77.93%), Space: 43.7 MB (6.35%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,17 +1,29 @@
 class Solution {
+    List<List<Integer>> output = new ArrayList<>();
+    int n, k;
+    
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> output = new ArrayList();
-        output.add(new ArrayList<Integer>());
-        for(int num :nums){
-            List<List<Integer>> newSubsets = new ArrayList();
-            for(List<Integer> curr: output){
-                newSubsets.add(new ArrayList<Integer>(curr){{add(num);}});
-            }
-            for(List<Integer>curr: newSubsets){
-                output.add(curr);
-                
-            }
+                 n = nums.length;
+        for(k=0; k<n+1; k++){
+            backtrack(0, new ArrayList<Integer>(), nums);
         }
         return output;
     }
+    
+    public void backtrack(int first, ArrayList<Integer> curr,int[] nums){
+        
+        // if the combination is done
+        if(curr.size()==k){
+            output.add(new ArrayList(curr));
+            return;
+        }
+        
+        for(int i=first; i<n; i++){
+            curr.add(nums[i]);
+            backtrack(i+1, curr, nums);
+            curr.remove(curr.size()-1);
+        }
+    }
+    
+    
 }
