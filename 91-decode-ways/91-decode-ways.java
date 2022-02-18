567b01e0d99e387bf6c478649f21bead7adac206
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-02-18 20:35:59 +0530

    Time: 2 ms (62.44%), Space: 42.5 MB (16.83%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,19 +1,13 @@
 class Solution {
     public int numDecodings(String s) {
-        Integer[] dp = new Integer[s.length()];
-        int ans = s.length()==0?0:decode(0,s,dp);
-        return ans;    
-    }
-    
-    private int decode(int i, String s, Integer dp[]){
-        int n=s.length();
-        if(i==n)return 1;
-        if(s.charAt(i)=='0')return 0;
-        if(dp[i]!=null)return dp[i];
-        int res = decode(i+1,s,dp);
-        if(i<n-1 && (s.charAt(i)=='1' || s.charAt(i)=='2' && s.charAt(i+1)<'7')){
-            res+=decode(i+2,s,dp);
-        }
-        return dp[i]=res;
-    }
-}
+       int n=s.length();
+        int[] dp=new int[n+1];
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            if(s.charAt(i)!='0') {
+                dp[i]=dp[i+1];
+                if(i<n-1&&(s.charAt(i)=='1'||s.charAt(i)=='2'&&s.charAt(i+1)<'7')) 
+					dp[i]+=dp[i+2];
+            }}
+        return dp[0];   
+}}
