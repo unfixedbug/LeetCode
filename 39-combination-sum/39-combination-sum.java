213e4420eddf371e75b21fbca7cf1632f93c7c3d
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-04-08 23:24:14 +0530

    Time: 7 ms (44.58%), Space: 45.1 MB (34.49%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,27 +1,27 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Set<Integer> set = new HashSet<>();
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        for(int el:candidates)set.add(el);
+        List<List<Integer>> ans = new ArrayList<>(); // answer list
+        List<Integer> temp = new ArrayList<>(); // current list
         
-        ArrayList<Integer> cand = new ArrayList<>(set);
+        ArrayList<Integer> cand = new ArrayList<>();
+        for(int ele:candidates)cand.add(ele);
+       
         Collections.sort(cand);
-         helper(cand,ans,target,0,temp);
+        helper(cand, ans, target,0, temp); // 0 is the starting index
         return ans;
     }
-    
-    public void helper(ArrayList<Integer> cand, List<List<Integer>> ans, int sum,int index, List<Integer> temp){
+    public void helper(ArrayList<Integer> cand, List<List<Integer>>ans, int sum, int index, List<Integer> curr ){
         if(sum==0){
-            ans.add(new ArrayList<>(temp));
+            ans.add(new ArrayList<>(curr));
             return;
         }
         for(int i=index;i<cand.size();i++){
-            if(sum -cand.get(i) >=0 ){//we want it
-                temp.add(cand.get(i));
-                helper(cand,ans,sum-cand.get(i),i,temp);
-                temp.remove(cand.get(i));
-            }
+            if(sum - cand.get(i) >=0){// we need more elements and we have the choice
+                curr.add(cand.get(i));
+                helper(cand,ans,sum-cand.get(i),i,curr); // take the element
+                curr.remove(cand.get(i));
+            }   
         }
+        
     }
 }
