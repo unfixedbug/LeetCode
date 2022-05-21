a8a2b3afe3bb30212c33635da69a726a182bb830
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-05-21 23:12:49 +0530

    Time: 87 ms (12.73%), Space: 45.3 MB (42.44%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+    if(amount<1) return 0;
+    return helper(coins, amount, new int[amount]);
+}
+
+private int helper(int[] coins, int rem, int[] count) { // rem: remaining coins after the last step; count[rem]: minimum number of coins to sum up to rem
+    if(rem<0) return -1; // not valid
+    if(rem==0) return 0; // completed
+    if(count[rem-1] != 0) return count[rem-1]; // already computed, so reuse
+    int min = Integer.MAX_VALUE;
+    for(int coin : coins) {
+        int res = helper(coins, rem-coin, count);
+        if(res>=0 && res < min)
+            min = 1+res;
+    }
+    count[rem-1] = (min==Integer.MAX_VALUE) ? -1 : min;
+    return count[rem-1];
+}
+}
