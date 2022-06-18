4ceeacceb3a7fff46248a77b469a5889afce4ce2
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-06-18 18:55:57 +0530

    Time: 3 ms (24.33%), Space: 49.4 MB (9.68%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,24 +1,25 @@
 class Solution {
     public int trap(int[] height) {
+        //left and right arrays are, largest element in the right, not next greater element
+        
         int n = height.length;
-        if(n<=2)return 0; // no water between two slabs
-        int water=0;
+        int left[] = new int[n],right[] = new int[n];
+        left[0] = height[0];
+        right[n-1]=height[n-1];
         
-        int left[] =new int[n], right[] = new int[n];
-        left[0] = Integer.MIN_VALUE;
+        for(int i=1;i<n;i++){
+            left[i] = Math.max(left[i-1],height[i]);
+        }
+        for(int i=n-2;i>=0;i--){
+            right[i] = Math.max(right[i+1],height[i]);
+        }
         
-        for(int i=1;i<n-1;i++){
-            left[i] = Math.max(left[i-1],height[i-1]); // largest bar to the left
-        }
-        right[n-1]  = Integer.MIN_VALUE;
-        for(int i=n-2;i >=0;i--){
-            right[i] = Math.max(right[i+1], height[i+1]); // dont take its own height
-        }
+        int ans=0;
         for(int i=0;i<n;i++){
             if(Integer.min(left[i], right[i]) > height[i]){
-                water += Integer.min(left[i],right[i])- height[i];
+                ans += Integer.min(left[i],right[i])- height[i];
             }
         }
-        return water;
+        return ans;
     }
 }
