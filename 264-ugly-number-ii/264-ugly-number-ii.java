b0f518f55d9996b2a3c1c32c5dacdd5927b028b6
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-06-29 15:07:59 +0530

    Time: 3 ms (93.34%), Space: 42.6 MB (41.46%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.java
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -1,17 +1,18 @@
 class Solution {
     public int nthUglyNumber(int n) {
         if(n==1)return 1;
-        int []dp = new int[n];
-        dp[0]=1;
-        int l2=0,l3=0,l5=0;
-        for(int i=1;i<n;i++){
-            dp[i] = Math.min(2*dp[l2],Math.min(3*dp[l3],5*dp[l5]));
-            if(dp[i]==2*dp[l2]){
-                l2++;
-            }
-            if(dp[i]==3*dp[l3]) l3++;
-            if(dp[i]==5*dp[l5])l5++;
+        int dp[] = new int[n+1];
+        dp[1]=1;
+        int m2=1,m3=1,m5=1;// start from the first element
+        for(int i=2;i<=n;i++){
+            // minimum multiple of previous ugly number,multplied by 2,3,5, not repeated
+            dp[i] = Math.min(2*dp[m2],Math.min(3*dp[m3], 5*dp[m5]));
+            
+            // see who donated the minimum element and accordingly incrrease the pointer
+            if(dp[i]==2*dp[m2])m2++;
+            if(dp[i]==3*dp[m3])m3++;
+            if(dp[i]==5*dp[m5])m5++;
         }
-        return dp[n-1];
+        return dp[n];
     }
 }
