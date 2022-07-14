f33c886d7346f91c239d4bf8de2d6827953d0923
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-07-14 14:54:07 +0530

    Added solution - LeetHub

---
+++ Array Subset of another array - GFG/array-subset-of-another-array.java
@@ -0,0 +1,68 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            long n = Long.parseLong(stt.nextToken());
+            long m = Long.parseLong(stt.nextToken());
+            long a1[] = new long[(int)(n)];
+            long a2[] = new long[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a1[i] = Long.parseLong(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                a2[i] = Long.parseLong(inputLine1[i]);
+            }
+            
+            
+            Compute obj = new Compute();
+            System.out.println(obj.isSubset( a1, a2, n, m));
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    public String isSubset( long a1[], long a2[], long n, long m) {
+        // a1 to be smaller and a2 to be larger
+        // if(n>m){
+        //     long[] temp = a2;
+        //     a2= a1;
+        //     a1 = temp;
+        // }
+        HashSet<Long> set = new HashSet<>();
+        
+        HashSet<Long> set2 = new HashSet<>();
+        for(long s:a2)set.add(s);
+        
+        for(long el:a1){
+            set2.add(el);
+        }
+        
+        
+        return set2.containsAll(set)?"Yes":"No";
+        
+    }
+}
