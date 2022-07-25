b779487d8a5b0ed41cc74a59df7985e762918385
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-07-25 21:38:49 +0530

    Time: 3 ms (79.06%), Space: 44.3 MB (30.32%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,22 +1,29 @@
 class Solution {
     int dp[][];
     public int minimumTotal(List<List<Integer>> tri) {
-        dp = new int[tri.size()][tri.size()];
+        int n = tri.size();
+        dp = new int[n][n];
         for(int a[]:dp){
             Arrays.fill(a,-1);
         }
-        return recur(tri,0,0);
+        // fill the last dp with values
+        for(int i=0;i<n;i++){
+            dp[n-1][i]=tri.get(n-1).get(i);
+        }
+        // tabulation is always opposite of memoisation so
+        // observe carefully, it is an lower triangular matrix
+        
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int ans = tri.get(i).get(j);
+                int down = dp[i+1][j]; /// this wiill reach max n-1,n-1, that is precomputed
+                int diagonal = dp[i+1][j+1];
+                dp[i][j] = ans + Math.min(down,diagonal);
+            }
+        }
+        return dp[0][0];
         
     }
     
-    private int recur(List<List<Integer>> tri, int i, int j){
-        if(i==tri.size()-1){
-            return tri.get(i).get(j);
-        }
-        if(dp[i][j]!=-1)return dp[i][j];
-        int ans=tri.get(i).get(j);
-        int down = recur(tri,i+1,j);//tri.get(i+1).get(j);
-        int diagonal = recur(tri,i+1,j+1);//tri.get(i+1).get(j+1);
-        return dp[i][j]=ans+Math.min(down,diagonal);
-    }
+   
 }
