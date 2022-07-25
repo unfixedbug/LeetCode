f74aa2d7ff8708b85e50e53ee11e2db00da8f217
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-07-25 18:15:57 +0530

    Time: 4 ms (35.76%), Space: 46 MB (40.98%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -7,16 +7,20 @@ class Solution {
         for(int a[]:dp){
             Arrays.fill(a,-1);
         }
-        return recursion(n-1,m-1,grid);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0)dp[i][j]=grid[i][j];
+                if(dp[i][j]!=-1)continue;
+                else if(i==0 && j!=0)dp[i][j] = grid[i][j]+dp[i][j-1];
+                else if(i!=0 && j==0)dp[i][j] = grid[i][j]+dp[i-1][j];
+                else{
+                    dp[i][j] = grid[i][j] + Math.min(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[n-1][m-1];
     }
     
-    private int recursion(int i, int j,int[][]grid){
-        
-        if(i==0 && j==0)return grid[0][0];
-        if(i<0|| j<0) return Integer.MAX_VALUE;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int up =  recursion(i-1,j,grid); // travelling from upside
-        int left = recursion(i,j-1,grid); // travelling from righside
-        return dp[i][j] = grid[i][j] + Math.min(up,left);
-    }
+    
 }
