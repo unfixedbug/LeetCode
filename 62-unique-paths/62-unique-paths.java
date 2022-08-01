bd116840454bc394d80e53e64f9701f101adebce
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-08-01 12:52:03 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (76.55%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -5,14 +5,16 @@ class Solution {
             Arrays.fill(a,0);
         }
         for(int i=0;i<m;i++){
+            // if last colm, then we have only one way, same for tha last roe
             dp[i][n-1]=1;
         }
         for(int i=0;i<n;i++){
             dp[m-1][i]=1;
         }
+        
         for(int i=m-2;i>=0;i--){
-            for(int j=n-2; j>=0; j--){
-                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            for(int j=n-2;j>=0;j--){
+            dp[i][j] = dp[i+1][j]+dp[i][j+1];
             }
         }
         return dp[0][0];
