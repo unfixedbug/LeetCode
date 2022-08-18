518a4c16007c3931f885d01015efa05e72f66ad9
Dhiraj Jadhav <60098505+unfixedbug@users.noreply.github.com>
2022-08-18 11:33:57 +0530

    Time: 68 ms (73.44%), Space: 91.8 MB (38.51%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,38 +1,26 @@
 class Solution {
     public int minSetSize(int[] arr) {
         int req = arr.length/2;
-        Set<Integer> set = new HashSet<>();
-        // contains -> l[0]-> number, l[1]-> value
         HashMap<Integer,Integer>map = new HashMap<>();
         for(int i=0;i<arr.length;i++){
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
-        //
-        List<Map.Entry<Integer,Integer>> list = new LinkedList<Map.Entry<Integer,Integer>>(map.entrySet());
-        
-        
-        Collections.sort(list, new Comparator<Map.Entry<Integer,Integer>>(){
-            public int compare(Map.Entry<Integer,Integer>o1, Map.Entry<Integer,Integer>o2){
-                return (o2.getValue()).compareTo(o1.getValue());
-            }
-        });
-            // sorted with values
-        HashMap<Integer,Integer> sorted = new LinkedHashMap<Integer,Integer>();
-        
-        for(Map.Entry<Integer,Integer> entry:list){
-            sorted.put(entry.getKey(),entry.getValue());
+        Integer freq[] = new Integer[map.size()];
+        int currIndex=0;
+        for(Map.Entry<Integer,Integer> en: map.entrySet()){
+            freq[currIndex++] = en.getValue();
         }
         
-        // sorted
-        int n=arr.length;
+        Arrays.sort(freq, Collections.reverseOrder());
+   
+        
+        int count=0;
         int ans=0;
-        for(Map.Entry<Integer,Integer> en:sorted.entrySet()){
-            if(n>req){
+        for(int e:freq){
+            if(count<req){
+                count+=e;
                 ans++;
-                n-=en.getValue();
-            }else{
-                break;
-            }
+            }else break;
         }
         return ans;
     }
